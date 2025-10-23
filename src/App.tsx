4007ec1f307c386ebc5601955b47c281c6976722
eduardoromerom <eduardo_274262@icloud.com>
2025-10-23 07:19:37 +0000

    chore: remove SyncProbe diagnostics and keep clean UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import SyncProbe from "./SyncProbe";
 export default function App(){
   return (
     <div style={{padding:16, fontFamily:"system-ui"}}>
       <h1>Ness Jewelry</h1>
-      <SyncProbe/>
     </div>
   );
 }
